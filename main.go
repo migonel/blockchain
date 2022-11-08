c6edd0cfca3a712163bf5b669eac68d2de9c9e65
Lucas <migonel55@gmail.com>
2022-11-08 18:19:56 -0300

    Wallet update: - Changed the code to a modular code - Add wallet package. It can create a wallet with a private key and a public key using ecdsa.

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"GoBlockchain/wallet"
+	"fmt"
+	"log"
+)
+
+func init() {
+	log.SetPrefix("Blockchain: ")
+}
+
+func main() {
+	w := wallet.NewWallet()
+	fmt.Println(w.PrivateKey())
+	fmt.Println(w.PublicKey())
+	fmt.Println(w.PrivateKeyStr())
+	fmt.Println(w.PublicKeyStr())
+}
